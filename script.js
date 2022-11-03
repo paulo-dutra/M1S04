c8ef972e4e7d986c8423e2ded6db6e0cc572985a
Pdutra <powdutra26@gmail.com>
2022-11-03 13:50:38 -0300

    Função para validação de operações

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ let dados_bancarios = [
         senha: "123654",
         conta: "9999",
         saldo: 150,
+    },
+    {
+        nome: "Cleber da Silva",
+        cpf: 12345678900,
+        celular: "(31) 9198-8191",
+        senha: "654abc",
+        conta: "1012",
+        saldo: 1500,
     }
 ]
 
-function gerador_numero_conta(){
+function gerador_numero_conta() {
     let numero_conta
     let numero_unico
-    do{
+    do {
         numero_unico = true
         numero_conta = JSON.stringify(Math.floor((Math.random() * 9000)) + 1000)
         dados_bancarios.forEach(objeto => {
-            if (objeto.conta == numero_conta){
+            if (objeto.conta == numero_conta) {
                 numero_unico = false
                 console.log("Numero conta")
             }
         })
-    }while (numero_unico == false)
+    } while (numero_unico == false)
     return numero_conta
 }
 
@@ -43,8 +51,8 @@ function valida_e_salva(event) {
             campo_vazio = true
         }
     });
-    if (campo_vazio == true) { 
-        return alert("É necessário preencher todos os campos para realizar o cadastro!") 
+    if (campo_vazio == true) {
+        return alert("É necessário preencher todos os campos para realizar o cadastro!")
     }
     //Validação senha e confirmação de senha
     if (valores[3] != valores[4]) {
@@ -64,25 +72,91 @@ Número da conta: ${novo_cadastro.conta}`)
 }
 
 function altera_tamanho_botoes(botao) {
-    document.getElementById('saque').classList.replace("ativo","operacao");
-    document.getElementById('deposito').classList.replace("ativo","operacao");
-    document.getElementById('saldo').classList.replace("ativo","operacao");
+    document.getElementById('saque').classList.replace("ativo", "operacao");
+    document.getElementById('deposito').classList.replace("ativo", "operacao");
+    document.getElementById('saldo').classList.replace("ativo", "operacao");
     botao.classList.replace("operacao", "ativo");
 }
 
-function desabilita_campo_valor(acao){
+function desabilita_campo_valor(acao) {
     document.getElementById("valor_op").disabled = acao;
-    
+}
+
+function valida_operacao(event) {
+
+    event.preventDefault();
+
+    //Verifica a operação selecionada e interrrompe as validações caso não haja operação
+    let operacao_selecionada = ""
+    const id_operacoes = ["saque", "deposito", "saldo"]
+    id_operacoes.forEach((id) => {
+        if (document.getElementById(id).className == "ativo") {
+            operacao_selecionada = id
+        }
+    }
+    )
+
+    if (operacao_selecionada == ""){
+        return alert("Selecione uma operação!")
+    }
+
+    //Percorre o array de contas até encontrar (ou não) o número de conta e confere a respectiva senha
+    let contaExiste = false
+    let senhaInvalida = true
+    let indice
+
+    for (indice = 0; indice < dados_bancarios.length; indice++) {
+        if (event.target.conta_op.value == dados_bancarios[indice].conta) {
+            contaExiste = true
+            if(event.target.senha_op.value == dados_bancarios[indice].senha){
+                senhaInvalida = false
+            }
+            break
+        }
+    }
+
+    if (!contaExiste) {
+        return alert("Este número de conta não está cadastrado no sistema.")
+    }
+
+    if (senhaInvalida) {
+        return alert("A senha está incorreta.")
+    }
+
+    //Chama a função correspondente à operação selecionada
+    switch (operacao_selecionada) {
+        case "saque":
+            operacao_saque();
+            break;
+        case "deposito":
+            operacao_deposito();
+            break;
+        case "saldo":
+            operacao_saldo();
+            break;
+    }
+}
+
+function operacao_saque() {
+    console.log("Saque")
+}
+
+function operacao_deposito() {
+    console.log("Deposito")
+}
+
+function operacao_saldo() {
+    console.log("Saldo")
 }
 
 const formulario_cadastro = document.getElementById('formulario-cadastro');
 formulario_cadastro.addEventListener('submit', valida_e_salva);
 
-const formulario_operacao =  document.getElementById("formulario-operacao");
-formulario_operacao.addEventListener('submit', (event) => event.preventDefault)
+const formulario_operacao = document.getElementById("formulario-operacao");
+formulario_operacao.addEventListener('submit', valida_operacao)
 
 const botao_saque = document.getElementById('saque');
-botao_saque.addEventListener('click',() => {
+botao_saque.addEventListener('click', () => {
     altera_tamanho_botoes(botao_saque)
     desabilita_campo_valor(false)
 });
